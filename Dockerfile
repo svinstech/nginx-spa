c77d7b66dbfcc2ec8a19a8182d4b408e78ab1934
Zach Krueger <zach.krueger@vouch.us>
2024-03-21 10:54:26 -0400

    enable embedded nginx config

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM nginx:stable-alpine
 
+ARG NGINX_CONF=./nginx.conf
+
 RUN sed -i '1idaemon off;' /etc/nginx/nginx.conf
 
-COPY nginx.conf /etc/nginx/conf.d/default.conf
+COPY ${NGINX_CONF} /etc/nginx/conf.d/default.conf
 
 CMD ["nginx"]
--- generate-nginx-configs.sh
+++ generate-nginx-configs.sh
@@ -1,3 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+generate_conf () {
+cat << EOF > $1
 server {
   listen 80 default_server;
 
@@ -12,10 +18,10 @@ server {
   # e.g. /, /user and /foo/bar will return index.html
   location / {
     add_header Cache-Control "no-store";
-    add_header Content-Security-Policy "frame-ancestors 'self'";
+    add_header Content-Security-Policy $2;
     add_header X-Content-Type-Options "nosniff";
     add_header X-XSS-Protection "1; mode=block";
-    try_files $uri $uri/index.html /index.html;
+    try_files \$uri \$uri/index.html /index.html;
   }
 
   # files
@@ -23,6 +29,12 @@ server {
   # e.g. /file.js returns a 404 if not found
   location ~ \.(?!html) {
     add_header Cache-Control "public, max-age=2678400";
-    try_files $uri =404;
+    try_files \$uri =404;
   }
 }
+EOF
+}
+
+generate_conf "nginx.conf" "\"frame-ancestors 'self'\""
+generate_conf "nginx-embedded-non-prod.conf" "\"frame-ancestors 'self' https://mock-embedded-partner.vouch-stg.us https://uat-mock-embedded-partner.vouch-stg.us https://mock-embedded-partner.vouch-dev.us\""
+generate_conf "nginx-embedded-prod.conf" "\"frame-ancestors 'self'\""
